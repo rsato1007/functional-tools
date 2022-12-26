376346890276535153026ff891664a6e229dab1f
rsato1007 <robertmsato@gmail.com>
2022-12-26 09:53:28 -0700

    creating update for TypeScript

---
+++ update/update.ts
@@ -0,0 +1,44 @@
+/**
+ * Updates property of object that's been passed.
+ * @param object - The object we're updating.
+ * @param key - The key to the object value we're updating.
+ * @param modify - The function that updates our value.
+ * @returns The updated object.
+ */
+const update = (
+    object: { [key: string]: any },
+    key: string,
+    modify: (arg: any) => any
+): { [key: string]: any } => {
+    if (object == null) {
+        return object;
+    } else {
+        object[key] = modify(object[key]);
+        return object;
+    }
+}
+
+/**
+ * Functional tool that updates a value in an nested object. User must know the path otherwise will not work.
+ * Requires update() in the same scope in order to work.
+ * @param object The object that needs to be altered.
+ * @param keys An array of keys to the path of the value to be updated.
+ * @param modify The function that updates the object's value.
+ * @returns The modified object.
+ */
+const nestedUpdate = (
+    object: { [key: string]: any },
+    keys: string[],
+    modify: (arg: any) => any
+): { [key: string]: any } => {
+
+    if (keys.length === 0) {
+        throw new Error("Atleast one key is required");
+    }
+    else if (keys.length === 1) {
+        update(object, keys[0], modify);
+        return object;
+    } else {
+        return nestedUpdate(object[keys[0]], keys.slice(1, keys.length), modify);
+    }
+}
